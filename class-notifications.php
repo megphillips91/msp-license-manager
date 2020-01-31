e8f1c1a494aba5eccdde2f63affe9df5918d4ec3
megphillips91 <meg.phillips91@gmail.com>
2020-01-31 13:05:17 -0500

    not a good copy, but syncing so I can clone it.

--- class-notifications.php
+++ class-notifications.php
@@ -3,9 +3,91 @@ namespace MSP_License_Pro;
 
 /**
  * License Notification sets up to send emails through wp_mail
- * @param (array) $users is an array('display_name', 'user_email');
- * @param (string) tell_admins | tell_meg |
+ * @param object $recipient is a WP User object
+ * @param string $from_email is a valid email for the reply to
+ * @param array $attachments is an array of attachments
  */
+
+add_shortcode('test_license_notification', 'test_license_notification_shortcode');
+
+function test_license_notification_shortcode(){
+  $user = get_user_by('ID', 1);
+  $message = new License_Expired_Email($user, 'meg.phillips@msp-media.org');
+  echo '<pre>'; var_dump($message); echo '</pre>';
+}
+
+class License_Notification {
+  public $to;
+  public $first_name;
+  public $reply_to;
+  public $message;
+  public $subject;
+  public $attachments;
+
+  public function __construct($recipient, $from_email, $attachments = NULL){
+    $this->headers = array('reply_to'=>$this->from_email, 'cc'=>'meg.phillips@msp-media.org');
+    $this->set_user_fields();
+    $this->set_message();
+    $this->set_subject();
+  }
+/*
+  public function send(){
+    $sent = wp_mail(
+      $this->to,
+      $this->subject,
+      $this->message,
+      $this->headers,
+      $this->attachments
+    );
+    return $sent;
+  }
+  */
+
+  private set_user_fields(){
+    $name = explode(' ', $user->data->display_name);
+    $this->first_name = $name[0];
+    $this->to = $user->data->user_email;
+  }
+
+  private function set_subject(){
+    $subject = '';
+    $this->subject = $subject;
+  }
+
+  private function set_message(){
+    $html = '';
+    $this->html = $html;
+  }
+
+
+}//end class
+
+
+class License_Expired_Email extends License_Notification {
+
+  private function set_message(){
+    $html = '<p>'.$this->first_name.',<p>';
+    $html .= '<p>When we last checked, your license for Charter Bookings Pro was issued on '.$issue_date.' for a term of 365 days. Just wanted to check in. </p></p>Are you planning to continue using Charter Bookings Pro?';
+    $html .= '<p>Meg Phillips<br>>Author Charter Bookings Pro<br>meg.phillips@msp-media.org</p>';
+    $this->message = $html;
+  }
+
+  private function set_subject(){
+    $subject = 'Charter Bookings Pro License Expiration';
+    $this->subject = $subject;
+  }
+
+}
+
+class Invalid_Domain_Email extends License_Notification {
+
+}
+
+
+
+/**
+ */
+/*
 
 class License_Notification {
   private set_message;
@@ -51,6 +133,6 @@ class License_Notification {
   }
 
 }
-
+*/
 
  ?>
